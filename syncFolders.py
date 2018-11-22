51d04103a45746683a1348c3de6e2c8ddf96dd3e
Feekoos <filip.petrinjak@gmail.com>
2018-11-22 21:34:55 +0100

    Add files via upload

---
+++ syncFolders.py
@@ -0,0 +1,15 @@
+﻿from dirsync import sync
+
+options = ''
+
+folders = {
+r'c:\Users\Filip\AppData\Local\Programs\Python\Python37\Scripts\win': r'd:\Python\Scripts_win',
+
+}
+
+print(len(folders))
+
+for x, y in folders.items():
+	source = x
+	destination = y
+	sync(source, destination, 'sync', ctime=True, twoway=True, create=True, purge=True)
